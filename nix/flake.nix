0cfb1fb53090b4866908f4551a48f25ecae5ac0c
Matthew Toohey <contact@mtoohey.com>
2024-02-03 22:29:08 -0500

    flake: depend on lean flake, add overlay and package output

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,18 +2,38 @@
   description = "lean4-protobuf";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
+    nixpkgs.follows = "lean/nixpkgs";
+    utils.follows = "lean/flake-utils";
+    lean.url = "github:leanprover/lean4/v4.4.0";
   };
 
-  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, utils, lean }@inputs: {
+    overlays.default = final: prev: {
+      # Use of `stdenv.hostPlatform` is fine cause buildLeanPackage doesn't
+      # appear to support cross compiling anyway since it also references
+      # `stdenv.hostPlatform`.
+      lean4-protobuf =
+        lean.packages.${final.stdenv.hostPlatform.system}.buildLeanPackage {
+          name = "Protobuf";
+          src = builtins.path { path = ./..; name = "lean4-protobuf-src"; };
+          # TODO: Add `executableName` field once protoc plugin is implemented.
+        };
+    };
+  } // utils.lib.eachDefaultSystem (system:
     let
-      pkgs = import nixpkgs { inherit system; };
-      inherit (pkgs) lean4 mkShell;
+      pkgs = import nixpkgs {
+        overlays = [ self.overlays.default ];
+        inherit system;
+      };
+      inherit (pkgs) lean4-protobuf mkShell;
+      leanPkgs = inputs.lean.packages.${system};
+      inherit (leanPkgs) lean;
     in
     {
+      packages.default = lean4-protobuf.modRoot;
+
       devShells.default = mkShell {
-        packages = [ lean4 ];
+        packages = [ lean ];
       };
     });
 }
